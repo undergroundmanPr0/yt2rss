0643e05b91cc8093c14b9797898cb99658cf6dfd
undergroundmanPr0 <sherriforwell@protonmail.ch>
2022-08-18 17:46:10 +0530

    minor script changes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-echo "this is for setting up your api"
+#!/bin/sh
+echo "This is for setting up your tool!"
 [[ ! -f go.mod ]] && go mod init yt2rss
 go get google.golang.org/api/youtube/v3
 go get google.golang.org/api/googleapi/transport
 go mod tidy
-go build && echo "Build complete! Now use test_script.sh"
+go build && echo "Build complete!"
 cp ./yt2rss /usr/bin/yt2rss
 cp config.json /etc/config.json
